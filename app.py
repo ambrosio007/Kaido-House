4f7ed4802fddd0ca809bfb2bd3ac29a417e5f33f
CaioFrancisco <caiofranciscocs@gmail.com>
2026-01-20 21:10:09 -0300

    ambiente virtual alterações

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from controller.user_controller import user_bp
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
+
+app.register_blueprint(user_bp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
