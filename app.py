971d8441fa934300da6884b058cc223445b0f58a
ambrosio007 <jose.a.neto40@aluno.senai.br>
2026-01-28 21:34:41 -0300

    modificação app.py para coreção de erro no render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from controller.user_controller import user_bp
+import os
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
@@ -7,4 +8,5 @@ app.secret_key = 'your_secret_key_here'
 app.register_blueprint(user_bp)
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', debug=False)
