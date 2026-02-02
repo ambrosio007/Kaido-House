6880960707c17a47fcca29ed5450516868222939
ambrosio007 <jose.a.neto40@aluno.senai.br>
2026-02-02 00:33:40 -0300

    atualização do fronte do cadastro de veiculos

--- model/veiculos_model.py
+++ model/veiculos_model.py
@@ -2,7 +2,7 @@ import uuid
 from datetime import datetime
 
 class VeiculoModel:
-    def __init__(self, user_id, marca, modelo, ano, km, cor, preco, descricao, fotos=None):
+    def __init__(self, user_id, marca, modelo, ano, km, cor, estado, preco, descricao, fotos=None):
         self.id = str(uuid.uuid4())
         self.user_id = user_id
         self.marca = marca
@@ -10,6 +10,7 @@ class VeiculoModel:
         self.ano = ano
         self.km = km
         self.cor = cor
+        self.estado = estado  # ✅ NOVO: novo, seminovo ou usado
         self.preco = preco
         self.descricao = descricao
         self.fotos = fotos or ""
@@ -25,6 +26,7 @@ class VeiculoModel:
             "ano": self.ano,
             "km": self.km,
             "cor": self.cor,
+            "estado": self.estado,  # ✅ NOVO
             "preco": self.preco,
             "descricao": self.descricao,
             "fotos": self.fotos,
